6afb8a01f2b19c87a23155e93a4d0fa52659890d
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-02-04 20:41:05 +0000

    create POST model for blog application

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from tkinter.tix import STATUS
+
+Status = (
+    (0, "Draft"),
+    (1, "Published")
+)
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['-created_on']
-
-    def __str__(self):
-        return self.title
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)

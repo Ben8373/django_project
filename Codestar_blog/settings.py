6a4c9ab7674ab1b328c6b7ad9968b61ba18f378e
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-02-04 21:23:56 +0000

    apply and run all migrations

--- Codestar_blog/settings.py
+++ Codestar_blog/settings.py
@@ -130,3 +130,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"]

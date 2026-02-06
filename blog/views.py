84f109850cb4576ceb83c075f798b2122394ad66
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-02-06 17:50:58 +0000

    Implement homepage with Django generic ListView

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,7 @@
-from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
-def home(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/home.html', {'posts': posts})
-
-def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
-
 class PostList(generic.ListView):
     model = Post
-    template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
+    queryset = Post.objects.all()
+    template_name = "blog/post_list.html"

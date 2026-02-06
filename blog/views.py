a889dc4474adb2be07a54925e1e5bf08ea4ff64c
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-02-06 21:33:23 +0000

    Fix template field name in home page
    - Change created_on to created_at to match Post model definition

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,9 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/post_list.html"
     context_object_name = 'posts'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/post_detail.html"
+    context_object_name = 'post'

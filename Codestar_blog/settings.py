90d4e0765f48868513ff3e31c85743100eab0980
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-02-06 17:04:48 +0000

    Remove accidental metadata from settings.py

--- Codestar_blog/settings.py
+++ Codestar_blog/settings.py
@@ -124,12 +124,3 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeinstitute-ide.net/",
-    "https://*.herokuapp.com"]

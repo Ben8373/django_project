14f0a5cfeaae12a9ccdfe99b71095e15201781a7
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-02-06 17:39:53 +0000

    Fix syntax error in settings.py

--- Codestar_blog/settings.py
+++ Codestar_blog/settings.py
@@ -15,19 +15,20 @@ from pathlib import Path
 import dj_database_url
 
 if os.path.isfile('env.py'):
-    
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-    BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-change-me-in-production")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -79,17 +80,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Codestar_blog.wsgi.application'
 
-import dj_database_url
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
- 
 
-
-# validation
-#https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -124,3 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]

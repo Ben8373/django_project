dc06d4124c4bba76e1de36fc3dd3f5ea64a7b785
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-02-07 16:56:17 +0000

    Improve PostAdmin with SummernoteModelAdmin and auto-slug generation

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_at')
-    list_filter = ('created_at', 'author')
-    search_fields = ('title', 'content')
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 admin.site.register(Post, PostAdmin)

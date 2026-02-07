063518496eebf0b3cfec478dfd839a76e271c636
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-02-07 16:40:07 +0000

    Fix admin URL routing from 'codestar/' to 'admin/'

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at')
+    list_display = ('title', 'slug', 'status', 'created_at')
     list_filter = ('created_at', 'author')
     search_fields = ('title', 'content')
 

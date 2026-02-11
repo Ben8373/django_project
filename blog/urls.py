a5551fcf757c046c67355cb9cb04651bf9738a1c
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-02-11 11:36:36 +0000

    Complete user authentication implementation

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
-    
-
 ]
